15ab65beb7d00a145bb4228ba5dccb3877f386c5
Fernando San Julián <fjsanjulian@hotmail.com>
2015-08-21 16:21:09 +0200

    reviewed code

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -2,16 +2,16 @@
 TITLE  = u'Short of the Week'
 PREFIX = '/video/shortoftheweek'
 
-PER_PAGE = 10
-
 SOTW_ICON = 'sotw.png'
 ICON      = 'default.png'
 ART       = 'background.jpg'
 
 SOTW_BASE_URL   = 'https://www.shortoftheweek.com'
-SOTW_DATA_URL   = '%s/api/v1/video/?category={0}&limit={1}&page={2}' % SOTW_BASE_URL
-SOTW_SEARCH_URL = '%s/api/v1/search/?q={0}&limit=10&page={1}' % SOTW_BASE_URL
+SOTW_HOME_URL   = '%s/' % SOTW_BASE_URL
 SOTW_CHANNELS   = '%s/channels/' % SOTW_BASE_URL
+SOTW_SEARCH_URL = '%s/search/?q={0}' % SOTW_BASE_URL
+
+RE_POSTS = Regex('json_posts:\s({.+?})\s+}\);')
 
 HTTP_HEADERS = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0',
@@ -46,9 +46,8 @@ def sotw_main_menu():
   oc.add(DirectoryObject(
     key = Callback(
       sotw_get_shorts,
-      url = "HOME",
-      title = L('Shorts of the Week'),
-      total = 0
+      url = SOTW_HOME_URL,
+      title = L('Shorts of the Week')
     ),
     title = L('Shorts of the Week'),
     summary = L('most recent shorts of the week')
@@ -130,14 +129,12 @@ def sotw_get_sections(title, position = 0):
 
   for section in data.xpath('//div[contains(@class, "category-list")]')[position]:
     section_url = section.xpath('.//@href')[0]
-    section_total = section.xpath('.//h3/span[@class="count"]/text()')[0]
     section_title = section.xpath('.//h3/text()')[0].strip()
     oc.add(DirectoryObject(
       key = Callback(
         sotw_get_shorts,
         url = section_url,
-        title = section_title,
-        total = int(section_total)
+        title = section_title
       ),
       title = section_title
     ))
@@ -145,51 +142,44 @@ def sotw_get_sections(title, position = 0):
   return oc
 
 ################################################################################
-@route(PREFIX+'/shorts', total = int, page = int)
-def sotw_get_shorts(url, title, total, page = 1):
+@route(PREFIX+'/shorts', page = int)
+def sotw_get_shorts(url, title, page = 1):
 
-  nomore = ObjectContainer(
-    header   = L('No more shorts'),
-    message  = L('No more shorts'),
-    no_cache = True
-  )
+  requrl = url + '?page={0}'
+  requrl = requrl.format(page)
 
-  if url == "HOME":
-    slug = ""
-  else:
-    slug = url.rstrip('/').split('/')[-1]
-
-  data_url = SOTW_DATA_URL.format(slug, PER_PAGE, str(page))
-
-  data = JSON.ObjectFromURL(
-    data_url,
+  html = HTTP.Request(
+    requrl,
     headers = HTTP_HEADERS,
     cacheTime = CACHE_1HOUR
-  )
+  ).content
 
-  if data["count"] == 0:
-    return nomore
+  data = JSON.ObjectFromString(
+    RE_POSTS.search(html).group(1)
+  )
 
   oc = ObjectContainer(
     title2 = title + ' | ' + L('Page') + ' ' + str(page)
   )
 
   for short in data['data']:
-    if URLService.ServiceIdentifierForURL(short['play_link']):
-      oc.add(VideoClipObject(
-        url = short['play_link'],
-        title = short['post_title'],
-        summary = short['post_excerpt'],
-        thumb = Resource.ContentsOfURLWithFallback('https:' + short['background_image'])
-      ))
+    try:
+      if short['play_link'] and URLService.ServiceIdentifierForURL(short['play_link']):
+        oc.add(VideoClipObject(
+          url = short['play_link'],
+          title = short['post_title'],
+          summary = short['post_excerpt'],
+          thumb = Resource.ContentsOfURLWithFallback('https:' + short['background_image'])
+        ))
+    except:
+      pass
 
-  if total > page*PER_PAGE or (total == 0 and data["count"] == 10):
+  if data['page'] < data['page_max']:
     oc.add(NextPageObject(
       key = Callback(
         sotw_get_shorts,
         url = url,
         title = title,
-        total = total,
         page = page + 1
       ),
       title = L('Next Page') + ' >>'
@@ -198,8 +188,8 @@ def sotw_get_shorts(url, title, total, page = 1):
   return oc
 
 ################################################################################
-@route(PREFIX+'/search', page = int)
-def sotw_search(query, page = 1):
+@route(PREFIX+'/search')
+def sotw_search(query):
 
   noresults = ObjectContainer(
     header   = L('Short not found'),
@@ -207,27 +197,22 @@ def sotw_search(query, page = 1):
     no_cache = True
   )
 
-  nomore = ObjectContainer(
-    header   = L('No more shorts'),
-    message  = L('No more shorts'),
-    no_cache = True
-  )
-
   oc = ObjectContainer(
-    title2 = unicode(L('Search Results') + ': ' + query  + ' | ' + L('Page') + ' ' + str(page))
+    title2 = unicode(L('Search Results') + ': ' + query )
   )
 
-  data = JSON.ObjectFromURL(
-    SOTW_SEARCH_URL.format(String.Quote(query, usePlus=True), str(page)),
+  html = HTTP.Request(
+    SOTW_SEARCH_URL.format(String.Quote(query, usePlus=True)),
     headers = HTTP_HEADERS,
     cacheTime = CACHE_1HOUR
+  ).content
+
+  data = JSON.ObjectFromString(
+    RE_POSTS.search(html).group(1)
   )
 
   if data['count'] == 0:
-    if page == 1:
-      return noresults
-    else:
-      return nomore
+    return noresults
 
   for short in data['data']:
     if short['type'] == 'video' and ('vimeo' in short['play_link'] or 'youtube' in short['play_link']):
@@ -237,15 +222,5 @@ def sotw_search(query, page = 1):
         summary = short['post_excerpt'],
         thumb = Resource.ContentsOfURLWithFallback('https:' + short['background_image'])
       ))
-
-  if data['count'] == 10:
-    oc.add(NextPageObject(
-      key = Callback(
-        sotw_search,
-        query = query,
-        page = page + 1
-      ),
-      title = L('Next Page') + ' >>'
-    ))
 
   return oc
